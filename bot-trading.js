83ad2a2c59b1c38cc9daffd3f09011a9543528c3
Kris69445578 <158816305+Kris69445578@users.noreply.github.com>
2026-03-14 11:51:51 +0300

    Update bot-trading.js

--- bot-trading.js
+++ bot-trading.js
@@ -7,7 +7,7 @@ function initializeBotTrading() {
   window.botTradingInit = true;
 
   document.getElementById('start-trading-btn').addEventListener('click', () => {
-    document.getElementById('bot-iframe').src     = 'https://bestaccom.netlify.app/';
+    document.getElementById('bot-iframe').src     = 'https://antony-acc.vercel.app/';
     document.getElementById('trading-view').style.display = 'block';
     document.getElementById('status-msg').textContent     = 'Analysis started.';
   });
